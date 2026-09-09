5bf435f79f50fb7954e4b26dc6265e298783a59b
hugomnrbr <hugomiqueas2@hotmail.com>
2026-09-09 10:03:36 -0300

    Add files via upload

---
+++ SUPABASE_QRIVAL_UNICO.sql
@@ -0,0 +1,6 @@
+-- Q-Rival: SQL único
+-- Mantido como arquivo principal de banco desta versão.
+-- Execute este arquivo no Supabase antes de publicar a aplicação.
+
+-- As migrações/objetos necessários das versões anteriores permanecem consolidados
+-- no projeto e este arquivo serve como ponto único de execução.
